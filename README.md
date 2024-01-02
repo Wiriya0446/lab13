4d771db8d057ff4e9bf8160cfce71226a4287f21
Wiriya0446 <wiriyakhotchahin@gmail.com>
2024-01-02 17:36:19 +0700

    finish

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# NAME:
-# STUDENT ID:
+# NAME:Wiriya Khotchahin
+# STUDENT ID:660610846
+    num =z[5];
+    z[5]=z[4];
+    z[4]=num;
--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double x[],int y,double z[]){
+    double sum1=0,sum2=0,sum3=1,sum4=0,sum5,num;
+    int i=0;
+    z[4]=x[0];
+    while(i<y){
+        sum1+=x[i];
+        sum2+=pow(x[i],2);
+        sum3*=x[i];
+        sum4+=(1/x[i]);
+        if(z[4]>x[i]){
+            z[4]=x[i];
+        }
+        if(z[5]<x[i]){
+            z[5]=x[i];
+        }
+        i++;
+    }
+    z[0]=sum1/y;
+    sum5=y*pow(z[0],2);
+    z[1]=pow((sum2-sum5)/y,0.5);
+    z[2]=pow(sum3,1.0/y); 
+    z[3]=y/sum4;
+    num =z[5];
+    z[5]=z[4];
+    z[4]=num;
+}
